6ca61dccb067589df8152bddff49359fa79a6e57
Ecaps <git@github.com:srkkk4/Ecaps.git>
2024-09-19 17:36:53 +0800

    9.19-1

--- test0915/source/DCZ/sum_g.cpp
+++ test0915/source/DCZ/sum_g.cpp
@@ -5,59 +5,15 @@
 using namespace std;
 int n,m;
 int a[MAXN];
+struct node{
+    int l; int r; int sum;
+}s[MAXN];
 int main(){
     cin>>n>>m;
     for (int i=1;i<=n;i++) cin>>a[i];
-    if (m==0){
-        int ans=0,mx=-INT_MAX;
-        for (int i=1;i<=n;i++){
-            ans+=a[i];
-            mx=max(mx,ans);
-            if (ans<0) ans=0;
-        }
-        cout<<mx<<endl;
-    } else if (m==1){
-        int mx=-INT_MAX;
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=i-1;j++){
-                swap(a[i],a[j]);
-                int ans=0;
-                for (int k=1;k<=n;k++){
-                    ans+=a[k];
-                    mx=max(mx,ans);
-                    if (ans<0) ans=0;
-                }
-                swap(a[i],a[j]);
-            }
-        }
-        cout<<mx<<endl;
-    } else if (m==2){
-        int mx=-INT_MAX;
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=i-1;j++){
-                for (int k=1;k<=n;k++){
-                    for (int l=1;l<=k-1;l++){
-                        swap(a[i],a[j]); swap(a[k],a[l]);
-                        int ans=0;
-                        for (int o=1;o<=n;o++){
-                            ans+=a[o];
-                            mx=max(mx,ans);
-                            if (ans<0) ans=0;
-                        }
-                        swap(a[i],a[j]); swap(a[k],a[l]);
-                        swap(a[k],a[l]); swap(a[i],a[j]);
-                        ans=0;
-                        for (int o=1;o<=n;o++){
-                            ans+=a[o];
-                            mx=max(mx,ans);
-                            if (ans<0) ans=0;
-                        }
-                        swap(a[k],a[l]); swap(a[i],a[j]);
-                    }
-                }
-            }
-        }
-        cout<<mx<<endl;
+    int tot=1,l=1;
+    for (int i=1;i<=n;i++){
+        
     }
     return 0;
 }
